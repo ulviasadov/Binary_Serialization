d987b26c60b627365bea1d25086f34c9b404c531
ulviasadov <ulviasadovv@gmail.com>
2025-04-27 01:46:01 +0400

    Add project files.

---
+++ Binary_Serialization/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Binary_Serialization
+{
+    public class Person
+    {
+        public Person() { }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Binary_Serialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Binary_Serialization
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person { Name = "Ulvi", Age = 24 };
+
+            string jsonString = JsonSerializer.Serialize(person);
+            File.WriteAllText("person.json", jsonString);
+
+            string readJson = File.ReadAllText("person.json");
+
+            Person? deserialize = JsonSerializer.Deserialize<Person>(readJson);
+            Console.WriteLine($"Name: {deserialize?.Name} Age: {deserialize?.Age}");
+        }
+    }
+}
